02fd962cf92af39fc340453a0912244908c0f70e
Hyegyu Lee <hgl1002@naver.com>
2017-06-17 19:04:27 +0900

    Punctuation replacer

---
+++ js/default.js
@@ -0,0 +1,17 @@
+
+var replacePunctuations = function() {
+	var entries = document.getElementsByClassName("entry");
+	for(var i in entries) {
+		var entry = entries[i];
+		var text = entry.innerHTML;
+		text = text.replace(/([^\w])\./g, function(match, g1) {return g1 + "<sup class=\"punc\">。</sup>";});
+		text = text.replace(/([^\w])\,/g, function(match, g1) {return g1 + "<sup class=\"punc\">、</sup>";});
+		entry.innerHTML = text;
+	}
+}
+
+var onDefaultLoad = function() {
+	replacePunctuations();
+}
+
+window.addEventListener('load', onDefaultLoad);
