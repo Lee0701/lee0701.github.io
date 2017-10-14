c0b574ddb95e51bc1b0424f1c464794f1940c899
Lee0701 <unconfigured@null.spigotmc.org>
2017-10-14 15:43:05 +0900

    Added comment form validation.

--- js/default.js
+++ js/default.js
@@ -167,6 +167,30 @@ var onKanchejaReady = function() {
 	if(hanjaMode == 3) replaceHanja(kancheja, kanchejaRegex);
 }
 
+var registerComment = function() {
+  var name = document.getElementById("comment-name");
+  var email = document.getElementById("comment-email");
+  var content = document.getElementById("comment-content");
+  var btn = document.getElementById("comment-submit");
+  var message = '';
+  var failed = false;
+  
+  if(!name.value) message += ", 이름";
+  if(!email.value) message += ", 전자 메일";
+  if(!content.value) message += ", 내용";
+  
+  if(message) {
+    message = message.substring(2);
+    message += "을 입력해 주십시오.";
+    btn.innerText = message;
+    btn.style.backgroundColor = "#d50000";
+    btn.style.color = "#ffffff";
+    return false;
+  } else {
+    return true;
+  }
+}
+
 var onDefaultLoad = function() {
 	
 	if(typeof(Storage) !== "undefined") {
